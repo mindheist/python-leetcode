b5edaf6e97769a0ec92d65a22f4edf7764122077
mindheist <dragonzsnake@gmail.com>
2015-03-29 20:59:52 -0700

    Number of 1 Bits

---
+++ Number_of_1_Bits.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/number-of-1-bits/
+
+# Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+# For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        return bin(n)[2:].count('1')
+
+    # the idea here is to use the inbuilt bin function -> that return the binary value of a given decimal number & use python 'count'
