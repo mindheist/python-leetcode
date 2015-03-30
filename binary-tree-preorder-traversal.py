3a22d69e2c2bd05df90bfaf2518a3a179d80abf6
mindheist <dragonzsnake@gmail.com>
2015-03-30 12:16:37 -0700

    + adding code for binary-tree-preorder-traversal

---
+++ binary-tree-preorder-traversal.py
@@ -0,0 +1,33 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def preorderTraversal(self, root):
+        # the overall idea of a pre-order traversal is 
+        # visit <    root     > ; print the value somewhere ; append to a stack
+        # < visit left  > 
+        # < visit right >
+        
+        if root == None:
+            return []
+        
+        # This problem is best solved using a stack to keep track of the nodes in a certain order
+        stack =[] 
+        stack.append(root)
+        result = []
+        
+        while len(stack) != 0:
+            current = stack.pop()
+            result.append(current.val)
+            if current.right != None :
+                stack.append(current.right)
+            if current.left  != None :
+                stack.append(current.left)
+            
+        return result
