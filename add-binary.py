7b51e3383ce57b3d7b22fd36aefa3f90ed5d76c2
mindheist <dragonzsnake@gmail.com>
2015-04-03 02:18:11 -0700

    + Add Binary solution

---
+++ add-binary.py
@@ -0,0 +1,19 @@
+# Given two binary strings, return their sum (also a binary string).
+
+# For example,
+# a = "11"
+# b = "1"
+# Return "100".
+
+# # Solution
+# ===========
+
+# a and b are converted to int , added and the Solution is converted back to binary
+# I guess I should revisit this and solve it without converting it back to decimal
+
+class Solution:
+    # @param a, a string
+    # @param b, a string
+    # @return a string
+    def addBinary(self, a, b):
+        return bin(int (a,2) + int (b,2))[2:]
