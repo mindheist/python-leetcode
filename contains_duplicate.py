23a49944e9bb7f19d81618c52ba6fc949ce2f51d
mindheist <dragonzsnake@gmail.com>
2016-04-17 16:47:20 -0700

    contains duplicate problem - added comments

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,6 +1,14 @@
 # https://leetcode.com/problems/contains-duplicate/
+# Given an array of integers, find if the array contains any duplicates.
+# Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 
-# hint : http://stackoverflow.com/questions/1323410/has-key-or-in
+# has_key() vs in  : http://stackoverflow.com/questions/1323410/has-key-or-in
+# If you had implement this solution using has_keys() and bumped into an error,thats
+# because has_Keys() has been deprecated in newer versions of python , using "in" is more pythonic
+
+# Solution
+# Create a dictionary or map datastructure , keep adding the numbers into the dictionary, bail out when you hit a dupe
+
 class Solution(object):
     def containsDuplicate(self, nums):
         self.my_dict={}
@@ -22,3 +30,7 @@ print my_solution.containsDuplicate(numbers_test_1)
 
 numbers_test_2 = [3,5,7,8,9,23,12,3]
 print my_solution.containsDuplicate(numbers_test_2)
+
+
+# COMPLEXITY
+# Well, this takes a single pass of the nums list , the solution is O(n) but takes additional space for the dictionary
