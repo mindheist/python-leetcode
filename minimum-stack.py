f42e0e6831e71a7b4d3cd186c85f6f5b09f645f1
mindheist <dragonzsnake@gmail.com>
2016-05-01 00:04:41 -0700

    minimum stack solution

---
+++ minimum-stack.py
@@ -0,0 +1,81 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.mainStack = []
+        self.minStack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+
+        if len(self.mainStack) == 0:
+
+            self.mainStack.append(x)
+            self.minStack.append(x)
+        else:
+            self.mainStack.append(x)
+            y = self.minStack[len(self.minStack)-1]
+            if x < y:
+                self.minStack.append(x)
+            else:
+                self.minStack.append(y)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.mainStack.pop()
+        self.minStack.pop()
+
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        #print len(self.mainStack)
+        # print self.minStack
+
+        return self.mainStack[len(self.mainStack)-1]
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[len(self.minStack)-1]
+
+
+    def print_stack(self):
+        print "minstack=" , self.minStack
+        print "mainStack" , self.mainStack
+        print "=========================="
+
+my_minstack = MinStack()
+my_minstack.print_stack()
+my_minstack.push(-2)
+my_minstack.print_stack()
+print my_minstack.top()
+my_minstack.print_stack()
+# my_minstack.push(6)
+# my_minstack.push(15)
+# my_minstack.push(56)
+# my_minstack.print_stack()
+# my_minstack.push(1)
+# my_minstack.print_stack()
+# my_minstack.push(0)
+# my_minstack.print_stack()
+# my_minstack.push(10)
+# my_minstack.print_stack()
+# my_minstack.pop()
+# my_minstack.print_stack()
+# my_minstack.pop()
+# my_minstack.print_stack()
+# my_minstack.push(80)
+# my_minstack.print_stack()
+
+#print my_minstack.getMin()
