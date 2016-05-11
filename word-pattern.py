8c49cebbbca693f59f6b84e43b54c2cb4bd82648
mindheist <dragonzsnake@gmail.com>
2016-05-11 14:50:59 -0700

    + added comments to word patterns

--- word-pattern.py
+++ word-pattern.py
@@ -1,3 +1,18 @@
+# https://leetcode.com/problems/word-pattern/
+
+# Given a pattern and a string str, find if str follows the same pattern.
+#
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+#
+# Examples:
+# pattern = "abba", str = "dog cat cat dog" should return true.
+# pattern = "abba", str = "dog cat cat fish" should return false.
+# pattern = "aaaa", str = "dog cat cat dog" should return false.
+# pattern = "abba", str = "dog dog dog dog" should return false.
+# Notes:
+# You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
+
+
 class Solution(object):
     def wordPattern(self, pattern, str):
         """
@@ -16,7 +31,7 @@ class Solution(object):
                     else:
                         my_dict[pattern[i]] = str.split()[i]
                 elif pattern[i] in my_dict:
-                    print pattern[i],my_dict[pattern[i]],str.split()[i]
+                    #print pattern[i],my_dict[pattern[i]],str.split()[i]
                     if my_dict[pattern[i]] != str.split()[i]:
                         return False
                     else:
