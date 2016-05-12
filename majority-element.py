7a2dbbc4e6f1987a5735d41bd3ee6b080ebb0a06
mindheist <dragonzsnake@gmail.com>
2016-05-11 21:58:35 -0700

    solution to add-digits (and misc)
    + solved Add-digits ( with tests)
    + comments for majority element
    + test infrastructure todo

--- majority-element.py
+++ majority-element.py
@@ -1,7 +1,7 @@
+# https://leetcode.com/problems/majority-element/
+
 # Given an array of size n, find the majority element. The majority element is the element that appears more than n/2  times.
 # You may assume that the array is non-empty and the majority element always exist in the array.
-
-
 
 class Solution:
     # @param num, a list of integers
---
+++ test-infrastructure/return-linked-list-datastructure.py
@@ -0,0 +1,2 @@
+# This is just a todo list at this point
+# Will have code that would generate Trees and Linked list that I could call in other programs for testing.
