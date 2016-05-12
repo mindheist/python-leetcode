ef8b9c47b447a492e52c8f336607af0844bbc4a4
mindheist <dragonzsnake@gmail.com>
2016-05-11 21:22:39 -0700

    + added some tests for power two

--- power-of-two.py
+++ power-of-two.py
@@ -6,6 +6,9 @@
 #  If you think about this in the 8421 notation , its nothing but 2^0 , 2^1 , 2^2 , 2^3 , 2^4 etc., etc.,
 #  And n always has exactly a single binary 1 set , and the rest of them are zeros
 #  And n-1 flips all the remaining 1 bits ; ANDing them results in Zero
+
+import unittest
+
 class Solution(object):
     def isPowerOfTwo(self, n):
         """
@@ -15,5 +18,23 @@ class Solution(object):
         if n == 0:
             return False
         else:
-        #Step 1: Use the above explained logic here 
+        #Step 1: Use the above explained logic here
             return (n & n-1)==0
+
+class TestPowerOfTwo(unittest.TestCase):
+    my_solution = Solution()
+
+    def test_two_power_three(self):
+        self.assertTrue(self.my_solution.isPowerOfTwo(8))
+
+    def test_not_a_power_of_two(self):
+        self.assertFalse(self.my_solution.isPowerOfTwo(1000))
+
+    def test_for_zero(self):
+        self.assertFalse(self.my_solution.isPowerOfTwo(0))
+
+    def test_for_one(self):
+        self.assertTrue(self.my_solution.isPowerOfTwo(1))
+
+if __name__ == "__main__":
+    unittest.main()
