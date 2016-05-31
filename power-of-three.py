d1865d52b93fccf50df385f7475a941ffbdc847a
mindheist <dragonzsnake@gmail.com>
2016-05-30 22:54:45 -0700

    Added a couple of test cases for power of three

--- power-of-three.py
+++ power-of-three.py
@@ -5,6 +5,8 @@
 
 # This is a pretty basic solution ; not sure how I would do it without loops
 
+
+import unittest
 class Solution(object):
     def isPowerOfThree(self, n):
         """
@@ -15,3 +17,15 @@ class Solution(object):
             while n%3==0:
                 n = n/3
         return n==1
+
+class TestPowerofThree(unittest.TestCase):
+    my_solution = Solution()
+
+    def test_default_pass(self):
+        self.assertTrue(self.my_solution.isPowerOfThree(27))
+
+    def test_default_fail(self):
+        self.assertFalse(self.my_solution.isPowerOfThree(30))
+
+if __name__ == "__main__":
+    unittest.main()
