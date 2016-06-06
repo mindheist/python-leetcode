b2da1bc769459d457cb63d320655e0ca0dbe0c91
mindheist <dragonzsnake@gmail.com>
2016-06-05 23:48:37 -0700

    Added tests for number palindrome

--- palindrome-number.py
+++ palindrome-number.py
@@ -15,9 +15,26 @@
 #
 # Subscribe to see which companies asked this question
 
+import unittest
 class Solution:
     # @return a boolean
     def isPalindrome(self, x):
         return str(x) == str(x)[::-1]
+
+class TestIsPalindrome(unittest.TestCase):
+    my_solution = Solution()
+
+    def test_default_pass(self):
+        self.assertTrue(self.my_solution.isPalindrome(121))
+
+    def test_not_a_palindrome(self):
+        self.assertFalse(self.my_solution.isPalindrome(123143))
+
+    def test_for_zero(self):
+        self.assertTrue(self.my_solution.isPalindrome(0))
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 # Just realized , I converted the integer to a string , so - I suppose this is not a valid solution in that case
